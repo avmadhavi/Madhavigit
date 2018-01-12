eddb44698dc8961a131fd042c2bd98e2ddf1bb52
avmadhavi <madhaviavhyd@gmail.com>
2018-01-12 12:33:54 +0530

    newgittt push

--- Gitassignments/src/Gitttt/Newgit1.java
+++ Gitassignments/src/Gitttt/Newgit1.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("himaaaasrii");
 	System.out.println("hemaaaaaa");
 	System.out.println("aniiiii");
+	System.out.println("pushhh");
 }
 }
